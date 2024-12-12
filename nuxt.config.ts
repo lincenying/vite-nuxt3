fdc19117ed37f8e0eea2b8c85fd67724903bd9b8
林岑影 <lincenying@qq.com>
2024-12-12 16:12:26 +0800

    chore: 将unocss样式独立成块

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -94,6 +94,21 @@ export default defineNuxtConfig({
                 },
             },
         },
+        build: {
+            rollupOptions: {
+                output: {
+                    manualChunks(id: string) {
+                        // 处理css分块
+                        if (id.includes('node_modules')) {
+                            return 'vendor'
+                        }
+                        if (id.includes('__uno.css')) {
+                            return 'unocss'
+                        }
+                    },
+                },
+            },
+        },
         // ...vite
     },
     hooks: {
