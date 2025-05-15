c1c01f8a289aa37aac35d4760080df28448a9c2a
LinCenYing <lincenying@qq.com>
2025-05-15 10:36:19 +0800

    chore: update deps

--- src/design.config.ts
+++ src/design.config.ts
@@ -11,7 +11,7 @@ export const minWindow = `${minWidth}Px`
 /** 为什么设置 100? 设置成便于计算, 计算rem时只需要将 设计稿的宽度 / 100 即可 */
 export const fontSize = 100
 
-export const charsetRemoval = function () {
+export function charsetRemoval() {
     return {
         postcssPlugin: 'internal:charset-removal',
         AtRule: {
