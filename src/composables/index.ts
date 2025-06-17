5ec79a36648a22ec8dcf031cb67a8a6510dbfafa
LinCenYing <lincenying@qq.com>
2025-06-17 16:08:24 +0800

    chore: 添加 useUrl 路由跳转

--- src/composables/index.ts
+++ src/composables/index.ts
@@ -2,6 +2,17 @@ import type { AnyFn } from '@vueuse/core'
 
 import md5 from 'md5'
 
+interface RouteQuery {
+    to?: string
+    t?: number
+    [propName: string]: any
+}
+
+interface RouteConfig {
+    path: string
+    query?: RouteQuery
+}
+
 export function useGlobal() {
     const ins = getCurrentInstance()!
 
@@ -51,6 +62,48 @@ export function useLockFn(fn: AnyFn, autoUnlock: boolean | 'auto' = 'auto') {
             throw e
         }
     }
+}
+
+/**
+ * 处理页面跳转逻辑。
+ *
+ * 此函数用于更新查询参数并触发页面跳转。它通过添加一个时间戳来确保URL的唯一性，从而避免浏览器缓存。
+ * 使用了Vue Router的钩子函数`useRouter`来获取路由实例，然后修改`to`对象的`query`属性并推动路由变化。
+ *
+/**
+ * @param path 目标路由的路径。
+ * @param query 查询参数对象，可能包含"to"和"t"属性。
+ * @param query.to 指定的查询参数"to"。
+ * @param query.t 指定的查询参数"t"。
+ */
+export function useUrl(path: string, query?: RouteQuery): void
+/**
+ * @param config 目标路由对象，包含路径和查询参数。
+ * @param config.path 目标路由的路径。
+ * @param config.query 查询参数对象，可能包含"to"和"t"属性。
+ * @param config.query.to 指定的查询参数"to"。
+ * @param config.query.t 指定的查询参数"t"。
+ */
+export function useUrl(config: RouteConfig): void
+export function useUrl(to: string | RouteConfig, config?: RouteQuery): void {
+    const router = useRouter()
+    let path: string = ''
+    let query: RouteQuery = {}
+
+    // 参数类型判断
+    if (typeof to === 'string') {
+        path = to
+        query = config || {}
+    }
+    else {
+        path = to.path || ''
+        query = to.query || {}
+        query.t = +Date.now()
+    }
+    router.push({
+        path,
+        query,
+    })
 }
 
 /**
